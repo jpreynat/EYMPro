ba5a513be0789d41c27bc0d238af39cba7be89c4
Johan <johan.preynat@gmail.com>
2015-03-13 13:08:16 +0000

    v1.0

---
+++ assets/pics.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  
+  $("#pictures").owlCarousel({
+    margin: 20,
+    loop: true,
+    center: true,
+    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplaySpeed: 1000,
+    autoplayHoverPause: true,
+    items: 3
+  });
+  
+  $("#pictures-sm").owlCarousel({
+    margin: 10,
+    loop: true,
+    center: true,
+    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplaySpeed: 1000,
+    autoplayHoverPause: true,
+    items: 4
+  });
+});
